1e843b203acef1a5a50c4210523eee5b3a912345
rigbyel <kickyouonce@gmail.com>
2024-02-04 01:00:13 +0300

    add handler for getting url by alias

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handlers/redirect"
 	"url-shortener/internal/http-server/handlers/url/save"
 	"url-shortener/internal/lib/logger/sl"
 	"url-shortener/internal/storage/sqlite"
@@ -46,6 +47,7 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Post("/url", save.New(log, storage))
+	router.Get("/{alias}", redirect.New(log, storage))
 
 	// starting server
 	log.Info("starting server", slog.String("addres", cfg.Address))
---
+++ internal/http-server/handlers/redirect/redirect.go
@@ -0,0 +1,59 @@
+package redirect
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	"url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+
+type URLGetter interface {
+	GetURL(alias string) (string, error)
+}
+
+func New(log *slog.Logger, urlGetter URLGetter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.redirect.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+			render.JSON(w, r, response.Error("invalid request"))
+			return
+		}
+
+		url, err := urlGetter.GetURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("url not found", "alias", alias)
+
+			render.JSON(w, r, response.Error("not found"))
+
+			return
+		}
+		if err != nil {
+			log.Info("error getting url", sl.Err(err))
+
+			render.JSON(w, r, response.Error("internal error"))
+
+			return
+		}
+
+		log.Info("got url", slog.String("url", url))
+
+		// redirect to the found url
+		http.Redirect(w, r, url, http.StatusFound)
+
+	}
+}
